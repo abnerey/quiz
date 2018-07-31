dc38606e959b8a63fd8a819370b8600580393cfc
ajuarez <nephilim1>
2018-07-31 16:41:38 -0600

    master: db

---
+++ dbTest.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE dbTest;
+USE dbTest;
+CREATE TABLE empleado (
+    id INT(10) AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    salario DECIMAL(10, 2) NOT NULL,
+    creacion DATE NOT NULL,
+    PRIMARY KEY(id)
+)
